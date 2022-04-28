14c2f4d7a7085258c080b06168002e56675ea729
Thomas Regourd <tom.reg@hotmail.fr>
2022-04-28 17:06:30 +0200

    modified flex

--- App.js
+++ App.js
@@ -172,62 +172,66 @@ export default function App() {
           <FontAwesome name="search" size={24} color="black" />
         </TouchableOpacity>
       </View>
-      <Text style={styles.city}>{weatherData?.name}</Text>
-      <View style={styles.tempContainer}>
-        <Text style={styles.currentTemp}>
-          {Math.round(weatherData?.main?.temp)}°C
-        </Text>
-        <Image
-          style={styles.weatherLogo}
-          source={{
-            uri: `http://openweathermap.org/img/w/${weatherData?.weather[0]?.icon}.png`,
-          }}
-        />
-        <Text style={styles.currentWeather}>
-          {weatherData?.weather[0]?.main}
-        </Text>
-        <View style={styles.tempMinMaxContainer}>
-          <Text style={styles.tempMinMax}>
-            Min {Math.round(weatherData?.main?.temp_min)}°C
+      <View style={styles.weatherContainer}>
+        <Text style={styles.city}>{weatherData?.name}</Text>
+        <View style={styles.tempContainer}>
+          <Text style={styles.currentTemp}>
+            {Math.round(weatherData?.main?.temp)}°C
           </Text>
-          <Text style={styles.tempMinMax}>
-            Max {Math.round(weatherData?.main?.temp_max)}°C
+          <Image
+            style={styles.weatherLogo}
+            source={{
+              uri: `http://openweathermap.org/img/w/${weatherData?.weather[0]?.icon}.png`,
+            }}
+          />
+          <Text style={styles.currentWeather}>
+            {weatherData?.weather[0]?.main}
+          </Text>
+          <View style={styles.tempMinMaxContainer}>
+            <Text style={styles.tempMinMax}>
+              Min {Math.round(weatherData?.main?.temp_min)}°C
+            </Text>
+            <Text style={styles.tempMinMax}>
+              Max {Math.round(weatherData?.main?.temp_max)}°C
+            </Text>
+          </View>
+        </View>
+        <View style={styles.windContainer}>
+          <FontAwesome5 name="wind" size={24} color="black" />
+          <Text style={styles.windText}>
+            Wind speed : {Math.round(weatherData?.wind?.speed)} km/h
           </Text>
         </View>
       </View>
-      <View style={styles.windContainer}>
-        <FontAwesome5 name="wind" size={24} color="black" />
-        <Text style={styles.windText}>
-          Wind speed : {Math.round(weatherData?.wind?.speed)} km/h
-        </Text>
-      </View>
-      <Text style={styles.forecastTitle}>5 days forecast</Text>
-      <ScrollView horizontal={true} style={styles.forecastScrollContainer}>
-        {forecastData.map((forecast) => {
-          return (
-            <View key={forecast.dt_txt} style={styles.forecast}>
-              <Text>{dayjs(forecast.dt_txt).hour()}h</Text>
-              <Image
-                style={styles.forecastLogo}
-                source={{
-                  uri: `http://openweathermap.org/img/w/${forecast?.weather[0]?.icon}.png`,
-                }}
-              />
-              <Text style={styles.currentForecast}>
-                {forecast?.weather[0]?.main}
-              </Text>
-              <View style={styles.forecastMinMaxContainer}>
-                <Text style={styles.forecastMinMax}>
-                  Min {Math.round(forecast?.main?.temp_min)}°C
+      <View style={styles.forecastContainer}>
+        <Text style={styles.forecastTitle}>5 days forecast</Text>
+        <ScrollView horizontal={true} style={styles.forecastScrollContainer}>
+          {forecastData.map((forecast) => {
+            return (
+              <View key={forecast.dt_txt} style={styles.forecast}>
+                <Text>{dayjs(forecast.dt_txt).hour()}h</Text>
+                <Image
+                  style={styles.forecastLogo}
+                  source={{
+                    uri: `http://openweathermap.org/img/w/${forecast?.weather[0]?.icon}.png`,
+                  }}
+                />
+                <Text style={styles.currentForecast}>
+                  {forecast?.weather[0]?.main}
                 </Text>
-                <Text style={styles.forecastMinMax}>
-                  Max {Math.round(forecast?.main?.temp_max)}°C
-                </Text>
+                <View style={styles.forecastMinMaxContainer}>
+                  <Text style={styles.forecastMinMax}>
+                    Min {Math.round(forecast?.main?.temp_min)}°C
+                  </Text>
+                  <Text style={styles.forecastMinMax}>
+                    Max {Math.round(forecast?.main?.temp_max)}°C
+                  </Text>
+                </View>
               </View>
-            </View>
-          );
-        })}
-      </ScrollView>
+            );
+          })}
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
@@ -246,103 +250,97 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
-    borderWidth: 1,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     alignItems: "center",
-    borderWidth: 1,
   },
   weatherLogo: {
     width: 100,
     height: 100,
-    borderWidth: 1,
   },
   currentWeather: {
     margin: 10,
     fontSize: 20,
-    borderWidth: 1,
   },
   tempContainer: {
-    //borderWidth: 1,
+    //
     flexDirection: "column",
     alignItems: "center",
-    borderWidth: 1,
   },
   tempMinMaxContainer: {
-    //borderWidth: 1,
+    //
     flexDirection: "row",
-    borderWidth: 1,
   },
   tempMinMax: {
     margin: 10,
     fontSize: 20,
-    borderWidth: 1,
   },
   currentTemp: {
     margin: 10,
     fontSize: 50,
-    borderWidth: 1,
   },
   city: {
     margin: 10,
     fontSize: 30,
-    borderWidth: 1,
   },
   windContainer: {
     flexDirection: "row",
     alignItems: "center",
-    borderWidth: 1,
   },
   windText: {
     margin: 10,
-    borderWidth: 1,
+  },
+
+  weatherContainer: {
+    flex: 4,
+    margin: 10,
+
+    alignItems: "center",
   },
 
   forecastContainer: {
+    flex: 2.5,
     margin: 10,
-    borderWidth: 1,
+    backgroundColor: "#f8f9fa",
+    alignItems: "center",
+    borderRadius: 10,
   },
 
   forecastScrollContainer: {
+    flex: 1,
     margin: 10,
     flexWrap: "wrap",
-    borderWidth: 1,
   },
 
   forecast: {
+    flex: 1,
     flexDirection: "column",
     alignItems: "center",
     margin: 5,
     borderRadius: 10,
     flexWrap: "wrap",
-    backgroundColor: "#e9ecef",
-    borderWidth: 1,
   },
   forecastLogo: {
     width: 50,
     height: 50,
-    borderWidth: 1,
   },
   currentForecast: {
     margin: 10,
     fontSize: 10,
-    borderWidth: 1,
   },
   forecastMinMaxContainer: {
     flexDirection: "column",
     alignItems: "center",
-    borderWidth: 1,
   },
   forecastMinMax: {
     margin: 2,
     fontSize: 10,
-    borderWidth: 1,
   },
 
   forecastTitle: {
     paddingTop: 20,
     fontSize: 20,
-    borderWidth: 1,
   },
 });
